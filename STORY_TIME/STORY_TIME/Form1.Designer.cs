bd89efe69c6c8b9bcd7b0e69ac1aba3e926c9438
JUSADAM1 <43390318+JUSADAM1@users.noreply.github.com>
2020-06-25 17:31:20 -0700

    Add files via upload

---
+++ STORY_TIME/STORY_TIME/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace STORY_TIME
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Title = new System.Windows.Forms.Label();
+            this.Start_Button = new System.Windows.Forms.Button();
+            this.Quit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.BackColor = System.Drawing.Color.Transparent;
+            this.Title.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.Color.Red;
+            this.Title.Location = new System.Drawing.Point(102, 38);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(770, 75);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "Welcome to Zombie or Survive";
+            // 
+            // Start_Button
+            // 
+            this.Start_Button.BackColor = System.Drawing.Color.Green;
+            this.Start_Button.Font = new System.Drawing.Font("Showcard Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Start_Button.ForeColor = System.Drawing.Color.Black;
+            this.Start_Button.Location = new System.Drawing.Point(350, 188);
+            this.Start_Button.Name = "Start_Button";
+            this.Start_Button.Size = new System.Drawing.Size(300, 59);
+            this.Start_Button.TabIndex = 1;
+            this.Start_Button.Text = "PLAY OR DIE!";
+            this.Start_Button.UseVisualStyleBackColor = false;
+            this.Start_Button.Click += new System.EventHandler(this.Start_Button_Click);
+            // 
+            // Quit
+            // 
+            this.Quit.BackColor = System.Drawing.Color.Red;
+            this.Quit.Font = new System.Drawing.Font("Showcard Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Quit.ForeColor = System.Drawing.Color.Black;
+            this.Quit.Location = new System.Drawing.Point(350, 324);
+            this.Quit.Name = "Quit";
+            this.Quit.Size = new System.Drawing.Size(300, 59);
+            this.Quit.TabIndex = 2;
+            this.Quit.Text = "QUITTER!!!!";
+            this.Quit.UseVisualStyleBackColor = false;
+            this.Quit.Click += new System.EventHandler(this.Quit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::STORY_TIME.Properties.Resources.wp2740272;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(927, 550);
+            this.Controls.Add(this.Quit);
+            this.Controls.Add(this.Start_Button);
+            this.Controls.Add(this.Title);
+            this.Name = "Form1";
+            this.Text = "ZOMBIE_DUDE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Button Start_Button;
+        private System.Windows.Forms.Button Quit;
+    }
+}
+
---
+++ STORY_TIME/STORY_TIME/Form2.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace STORY_TIME
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Directions = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Montserrat Subrayada", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(163, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(501, 73);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "instructions";
+            // 
+            // Directions
+            // 
+            this.Directions.AutoSize = true;
+            this.Directions.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Directions.Location = new System.Drawing.Point(40, 159);
+            this.Directions.Name = "Directions";
+            this.Directions.Size = new System.Drawing.Size(713, 29);
+            this.Directions.TabIndex = 1;
+            this.Directions.Text = "The main point of this game is to make choices based on clicking  a button.";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.OliveDrab;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(147, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(518, 156);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "LETS PLAY!!!!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(781, 502);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Directions);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Montserrat Subrayada", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form2";
+            this.Text = "instructions";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Directions;
+        private System.Windows.Forms.Button button1;
+    }
+}
